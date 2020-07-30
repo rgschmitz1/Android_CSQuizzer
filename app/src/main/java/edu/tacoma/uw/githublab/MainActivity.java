65ad6b64c5c4b276266c10da52acf158abc450b0
Michael Pham <michaelphamn@yahoo.com>
2020-07-29 20:39:20 -0700

    Login, Logout, Register, Recovery email, New Password

--- app/src/main/java/edu/tacoma/uw/githublab/MainActivity.java
+++ app/src/main/java/edu/tacoma/uw/githublab/MainActivity.java
@@ -1,19 +1,43 @@
 package edu.tacoma.uw.githublab;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.View;
-
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class MainActivity extends AppCompatActivity {
-
+    BottomNavigationView bottomNavigation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        openFragment(HomeFragment.newInstance("", ""));
     }
-
-    public void launchPhucActivity(View view) {
-        // Added outline of function to launch PhucActivity
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+        new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+                        openFragment(HomeFragment.newInstance("", ""));
+                        return true;
+                    case R.id.navigation_sms:
+                        openFragment(SmsFragment.newInstance("", ""));
+                        return true;
+                    case R.id.navigation_notifications:
+                        openFragment(NotificationFragment.newInstance("", ""));
+                        return true;
+                }
+                return false;
+            }
+        };
 }
